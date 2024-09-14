52243ade4af9d66187fdaa522996eb5528876cfd
Yanai1005 <tatsuya871_1005@icloud.com>
2024-09-14 11:56:14 +0900

    [wip] #10 初めてソケットやってみた

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
+from flask_socketio import SocketIO, emit
 import threading
-import joycon_module  # 修正したファイル名に合わせてインポート
+import joycon_module
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
-# Joy-Conのデータを保持する変数
+# Joy-Conのデータを保持するグローバル変数
 joycon_data = {"accel": {}, "score": 0}
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return send_from_directory('.', 'index.html')
 
 @app.route('/joycon')
 def get_joycon_data():
-    global joycon_data  # グローバル変数を参照
-    print("Joy-Con data request received")
-    # 現在のJoy-Conデータを返す
+    global joycon_data
     return jsonify(joycon_data)
 
 def start_joycon_data_acquisition():
     global joycon_data
     joycon_module.start_joycon_data_acquisition(joycon_data)
+
+def joycon_data_update_thread():
+    while True:
+        socketio.emit('joycon_update', joycon_data)
+        socketio.sleep(0.5)
 
 if __name__ == '__main__':
     # Joy-Conのデータ取得を別スレッドで開始
@@ -28,5 +33,7 @@ if __name__ == '__main__':
     joycon_thread.daemon = True
     joycon_thread.start()
 
-    # Flaskサーバーをポート5001で実行
-    app.run(port=5001)
+    # WebSocketのデータ送信スレッドを開始
+    socketio.start_background_task(target=joycon_data_update_thread)
+
+    socketio.run(app, port=5001)
